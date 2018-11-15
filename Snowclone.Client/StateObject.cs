12b19dd975969279e7ad277d3bb3f43ed05e3134
Alex <alexmorson95@gmail.com>
2018-11-14 22:16:43 -0600

    first commit. Prelminary architecture setup

---
+++ Snowclone.Client/StateObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Snowclone.Client
+{
+    public class StateObject
+    {
+        public Socket WorkSocket = null;
+        public const int BufferSize = 256;
+        public byte[] Buffer = new byte[BufferSize];
+        public StringBuilder stringBuilder = new StringBuilder();
+    }
+}
---
+++ Snowclone.Server/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
+
+namespace Snowclone.Server
+{
+    public class Server
+    {
+        public string IP;
+        public int Port;
+
+        //private ConcurrentBag<Member> memberlist
+    }
+}
